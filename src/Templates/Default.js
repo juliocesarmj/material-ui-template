28f5a5f1f4bf731db0cb1ef8720315ef2fe5dcbc
MPRJ <MPRJ@MPRJ-135513.pgj.rj.gov.br>
2021-06-07 11:38:40 -0300

    adicionando template default

---
+++ src/Templates/Default.js
@@ -0,0 +1,23 @@
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Header from '../partials/Header/Header';
+
+const useStyles = makeStyles(() => ({
+  container: {
+    padding: '15px 0',
+    background: 'red',
+  },
+}));
+
+const Default = ({ children }) => {
+  const classes = useStyles();
+  return (
+    //algo aqui
+    <>
+      <Header />
+      <Container className={classes.container}>{children}</Container>
+    </>
+  );
+};
+export default Default;
