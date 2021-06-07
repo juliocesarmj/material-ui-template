a4783beaa81960b1423b48fd3fe9f52f54de1299
MPRJ <MPRJ@MPRJ-135513.pgj.rj.gov.br>
2021-06-07 13:50:44 -0300

    adicionando biblioteca classnames

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,8 +1,21 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
 import CustomerCard from '../components/CustomerCard';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  card: {
+    margin: theme.spacing(2),
+    background: 'red',
+  },
+}));
 const Customers = () => {
+  const classes = useStyles();
   const [customers, setCustomers] = useState([]);
 
   useEffect(() => {
@@ -11,19 +24,23 @@ const Customers = () => {
       setCustomers(data);
     });
   }, []);
-  console.log(customers);
+
   return (
     <>
       <h1>Customers</h1>
-      {customers.map((item) => (
-        <CustomerCard
-          key={item.id}
-          name={item.first_name}
-          lastname={item.last_name}
-          email={item.email}
-          avatar={item.avatar}
-        />
-      ))}
+      <Grid container>
+        {customers.map((item) => (
+          <Grid item xs={12} md={4} key={item.id}>
+            <CustomerCard
+              name={item.first_name}
+              lastname={item.last_name}
+              email={item.email}
+              avatar={item.avatar}
+              className={classes.card}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 };
