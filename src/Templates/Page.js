69da1fa942346fdc2a701f5eca30e376a43fbfb5
MPRJ <MPRJ@MPRJ-135513.pgj.rj.gov.br>
2021-06-07 14:55:27 -0300

    adicionando a funcionalidade do menu drawer

--- src/Templates/Page.js
+++ src/Templates/Page.js
@@ -1,14 +1,14 @@
-import { makeStyles } from '@material-ui/core/styles';
+//import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles(() => ({
-  container: {
-    padding: '15px 0',
-  },
-}));
+// const useStyles = makeStyles(() => ({
+//   container: {
+//     padding: '15px 0',
+//   },
+// }));
 
 const Page = ({ title, Component }) => {
-  const classes = useStyles();
+  // const classes = useStyles();
   return (
     //algo aqui
     <>
